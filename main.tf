dc1623fa7ec7b099893fee0f73d8d5e91330612c
aambadkar <khodaskar.ankita@gmail.com>
2023-10-13 11:06:19 +0300

    expense script

--- main.tf
+++ main.tf
@@ -8,8 +8,11 @@ variable "parameters" {
   default = [
     {name = "prod.rds.master-password", value = "expenseprod1234", type = "SecureString"},
     {name = "prod.rds.master_username", value = "expense", type = "String"},
-    {name = "expense.frontend.backend_url", value = "http://backend-prod.devopsa17.online/", type = "String"},
+    {name = "prod.expense.frontend.backend_url", value = "http://backend-prod.devopsa17.online/", type = "String"},
     {name = "prod.rds.endpoint", value = "prod-mysql.cluster-cgl0nqywutw1.us-east-1.rds.amazonaws.com", type = "String"}
+
+    ##dev
+    {name = "dev.expense.frontend.backend_url", value = "http://backend-dev.devopsa17.online/", type = "String"},
 
   ]
 
